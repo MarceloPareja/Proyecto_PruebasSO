610c6beb5330446c62fc63302b20519e3d01897b
Marcelo <marcelegacy32@gmail.com>
2025-12-10 10:08:16 -0500

    Arreglo dependencias de pruebas

--- Backend/tests/Account/AccountTestDelete.test.js
+++ Backend/tests/Account/AccountTestDelete.test.js
@@ -1,20 +1,25 @@
 const request = require('supertest');
 const app = require('../../app'); 
+const mongoose = require('mongoose');
 const { loginAndGetToken } = require('../helper/loginhelper');
 
 describe('Account API Tests Delete', () => {
 let authToken = '';
 
   beforeAll(async () => {
+    await mongoose.connect(process.env.MONGODB_URI);
    authToken = await loginAndGetToken();
   });
 
   it('Prueba de eliminación de una cuenta por ID - Éxito', async () => {
     const response = await request(app)
-      .delete('/legalsystem/accounts/delete/1') 
+      .delete('/legalsystem/accounts/delete/19') 
       .set('Authorization', `Bearer ${authToken}`);
     expect(response.statusCode).toBe(200);
     expect(response.body.deletedCount).toBe(1); 
   });
   
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 });
